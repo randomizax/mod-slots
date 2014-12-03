a34f5d65a9941fb590f2abb968c4122bb389eb75
Random Xyzzy <rndmxyzzy@gmail.com>
2014-12-03 14:38:33 +0900

    Update version string

--- mod-slots.user.js
+++ mod-slots.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-mod-slots@randomizax
 // @name           IITC plugin: Portal Mod Slots Availability
 // @category       Layer
-// @version        0.1.4.20141203.140314
+// @version        0.1.4.20141203.143714
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://rawgit.com/randomizax/mod-slots/latest/mod-slots.meta.js
 // @downloadURL    https://rawgit.com/randomizax/mod-slots/latest/mod-slots.user.js
-// @description    [randomizax-2014-12-03-140314] Show mod slots vacancy
+// @description    [randomizax-2014-12-03-143714] Show mod slots vacancy
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
